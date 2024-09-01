bc2d38380adf6af0e1002a059ee059c5c85d82ff
pankaj-gitcode <pankajaditya75@gmail.com>
2024-09-01 10:28:45 +0530

    type array with multiple arguments

--- 1_code/node-app/src/arrays.ts
+++ 1_code/node-app/src/arrays.ts
@@ -10,7 +10,30 @@ const arrFunc = (arr:arryType[])=>{
     console.log(`type is: ${typeof arr}`)
 }
 
-arrFunc(arrayNum);
-arrFunc(arrayStr);
-arrFunc(arryStrNum);
-arrFunc(arreyBool);
+// arrFunc(arrayNum);
+// arrFunc(arrayStr);
+// arrFunc(arryStrNum);
+// arrFunc(arreyBool);
+
+const arrFunc1 = (arr:object)=>{console.log(`type of ${arr} is ${typeof arr}`)}
+// arrFunc1(arrayNum);
+
+//********************************************************************** */
+type user = {
+    name: string;
+    age:number
+}
+
+const emp = (arr: user[])=>{
+    //filter out the user having ag>18
+    
+    return arr.filter(elem=>elem.age > 18);
+}
+
+const empFunc = emp([
+    {name: 'Raj',age:21}, 
+    {name: 'Kmr',age:2}, 
+    {name: 'Mac',age:3}
+])
+
+console.log(empFunc)
